8959be2657809aa3fa6f7a0d8bc1c3a50d7f76a1
Abd-Wadood <wadoodabdul394@gmail.com>
2026-05-18 18:35:58 +0500

    supplier

--- BranchPOS.Tests/obj/Debug/net10.0/BranchPOS.Tests.AssemblyInfo.cs
+++ BranchPOS.Tests/obj/Debug/net10.0/BranchPOS.Tests.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BranchPOS.Tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f511afc09ac75837e7cd6be598306f833cc6e74b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+54f0a1e1ad2aeb8e0a84d0e20382957018b10f6d")]
 [assembly: System.Reflection.AssemblyProductAttribute("BranchPOS.Tests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BranchPOS.Tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- BranchPOS/obj/Debug/net10.0/BranchPOS.AssemblyInfo.cs
+++ BranchPOS/obj/Debug/net10.0/BranchPOS.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BranchPOS")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b9a1ae4409afcd7de2ada7f53d0604c0e714e651")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+54f0a1e1ad2aeb8e0a84d0e20382957018b10f6d")]
 [assembly: System.Reflection.AssemblyProductAttribute("BranchPOS")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BranchPOS")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
